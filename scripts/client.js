b1dadc8a9a863a7f465e5c26f79a4e19e00d5571
p4suhag <p4suhag@gmail.com>
2016-09-08 17:05:01 +0530

    modify code & change home font

--- scripts/client.js
+++ scripts/client.js
@@ -2,22 +2,29 @@ $('.js-connect-bar').click(function() {
     $('.js-connect').animate({
         // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
         left: '55%'
-    }, 800);
-    $('.home-gallery-box').animate({
+    }, 1100);
+    $('.home-gallery').animate({
         opacity: '0'
-    },500);
+    },700);
 });
 $('.js-home-bar').click(function() {
     $('.js-connect').animate({
         // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
         left: '100%'
-    }, 800);
-    $('.home-gallery-box').animate({
+    }, 1000);
+    $('.home-gallery').animate({
         opacity: '1'
-    },700);
+    },1100);
 });
 $('.js-work-bar').click(function() {
     $('.work-page').css('visibility', 'visible');
+    $('.project-about').animate({
+        opacity: '1'
+    },900);
+    $('.geekfunnel-coverpic').animate({
+        left: '0',
+        opacity: '1'
+    },1100);
 });
 $('.js-close-button').click(function() {
     $('.work-page').css('visibility', 'hidden');
