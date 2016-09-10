aefbcd23639ae6ad37ba62a871556b036abed8f8
p4suhag <p4suhag@gmail.com>
2016-09-10 14:42:39 +0530

    add transition to work page

--- scripts/client.js
+++ scripts/client.js
@@ -1,28 +1,28 @@
-$('.js-connect-bar').click(function() {
-    $('.js-connect').animate({
-        // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
-        left: '55%'
-    }, 1100);
-    $('.home-gallery').animate({
-        opacity: '0'
-    },700);
-});
-$('.js-home-bar').click(function() {
-    $('.js-connect').animate({
-        // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
-        left: '100%'
-    }, 1000);
-    $('.home-gallery').animate({
-        opacity: '1'
-    },1800);
-});
+// $('.js-connect-bar').click(function() {
+//     $('.js-connect').animate({
+//         // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
+//         left: '55%'
+//     }, 1100);
+//     $('.home-gallery').animate({
+//         opacity: '0'
+//     },700);
+// });
+// $('.js-home-bar').click(function() {
+//     $('.js-connect').animate({
+//         // box-shadow: -2px 0px 2px 0 rgba(10, 10, 10, 0.1),
+//         left: '100%'
+//     }, 1000);
+//     $('.home-gallery').animate({
+//         opacity: '1'
+//     },1800);
+// });
 $('.js-work-bar').click(function() {
     $('.js-work-page').css('visibility', 'visible');
     $('.js-geek-about').animate({
         opacity: '1',
         top: '0'
     },900);
-    $('.geekfunnel-coverpic').animate({
+    $('.js-geekfunnel-coverpic').animate({
         left: '0',
         opacity: '1'
     },1100);
@@ -33,27 +33,41 @@ $('.js-close-button').click(function() {
 
 $('.js-work-page').scroll(function(){
    var pageScroll = $('.js-work-page').scrollTop(),
-       geeksubposition = $('.geekfunnel-subcontainer').offset().top;
+       geeksubposition = $('.geekfunnel-subcontainer').offset().top,
+       weaponxsubposition = $('.weaponx-container').offset().top;
 
-    // var docScroll = $(this).scrollTop(), 
-    //     boxCntOfset = $(".box-container").offset().top - 100;
     
-    // console.log(docScroll);
-    // console.log(pageScroll);
-    // console.log(geeksubposition);
     if (pageScroll >= geeksubposition) {
         console.log('hello');
-        $('.geekfunnel-pic').animate({
+        $('.js-geeksubpic1').animate({
+            opacity: '1',
+            left: '0'
+        },1100);
+        $('.js-geeksubinfo1').animate({
+            opacity: '1',
+            left: '0'
+        },1300);
+    }
+    if (pageScroll >= geeksubposition + 400) {
+        console.log('hello');
+        $('.js-geeksubpic2').animate({
+            opacity: '1',
+            left: '0'
+        },1100);
+        $('.js-geeksubinfo2').animate({
+            opacity: '1',
+            left: '0'
+        },1500);
+    }
+    if (pageScroll >= weaponxsubposition + 700) {
+        $('.js-weaponx-about').animate({
+            opacity: '1',
+            top: '0'
+        },1100);
+        $('.js-weaponx-coverpic').animate({
+            left: '0',
             opacity: '1'
         },1100);
     }
-    //when rich top of boxex than fire
-    // if(docScroll >= boxCntOfset ) {
-
-    //   $("#first").fadeIn(200)
     
-    // } else {
-    //  $("#first").fadeOut(200)
-    
-    // }
 }) 
